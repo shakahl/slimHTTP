28cb84957c3c91095e76e7cae8c6ecefe63f2e50
Anton Hvornum <anton.feeds+github@gmail.com>
2020-06-26 14:10:58 +0200

    Added some more documentation.

--- slimhttpd.py
+++ slimhttpd.py
@@ -238,6 +238,14 @@ class Events():
 	NOT_YET_IMPLEMENTED = 0b00000000
 
 class ROUTE_HANDLER():
+	"""
+	Stub function that will act as a gateway between
+	@http.<function> and the in-memory route that is stored.
+
+	I might be using annotations wrong, but this will store
+	a route (/url/something) and connect it with a given function
+	by the programmer.
+	"""
 	def __init__(self, route):
 		self.route = route
 		self.parser = None
