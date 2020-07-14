56044da6fd49eee8a8137f8deba669e0bbb11538
Anton Hvornum <anton.feeds+github@gmail.com>
2020-07-14 10:47:29 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if not os.path.isdir('./slimHTTP/examples'):
 
 setuptools.setup(
 	name="slimHTTP",
-	version="1.0.1rc3",
+	version="1.0.1rc4",
 	author="Anton Hvornum",
 	author_email="anton@hvornum.se",
 	description="A minimalistic non-threaded HTTP server written in Python. Supports REST & WebSockets¹.",
